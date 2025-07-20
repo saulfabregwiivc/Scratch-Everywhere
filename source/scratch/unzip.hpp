405951585640192f64a401df7b2bd4b8fbe686c0
NateXS <thecoolgamer0003@gmail.com>
2025-07-20 12:23:23 -0500

    fix unzipProject()

--- source/scratch/unzip.hpp
+++ source/scratch/unzip.hpp
@@ -11,9 +11,9 @@ class Unzip {
 
     static mz_zip_archive zipArchive;
     static std::vector<char> zipBuffer;
-    
-    static void openScratchProject(void* arg){
-      
+
+    static void openScratchProject(void *arg) {
+
         std::ifstream file;
         int isFileOpen = openFile(&file);
         if (isFileOpen == 0) {
@@ -67,50 +67,48 @@ class Unzip {
                 return project_json;
             }
 
-    if(projectType != UNZIPPED){
-        // read the file
-        std::cout<<"Reading SB3..."<<std::endl;
-        std::streamsize size = file->tellg(); // gets the size of the file
-        file->seekg(0,std::ios::beg); // go to the beginning of the file
-        zipBuffer.resize(size);
-        if (!file->read(zipBuffer.data(), size)){
-            return project_json;
+            if (projectType != UNZIPPED) {
+                // read the file
+                std::cout << "Reading SB3..." << std::endl;
+                std::streamsize size = file->tellg(); // gets the size of the file
+                file->seekg(0, std::ios::beg);        // go to the beginning of the file
+                zipBuffer.resize(size);
+                if (!file->read(zipBuffer.data(), size)) {
+                    return project_json;
+                }
+
+                // open ZIP file from the thing that we just did
+                std::cout << "Opening SB3 file..." << std::endl;
+                memset(&zipArchive, 0, sizeof(zipArchive));
+                if (!mz_zip_reader_init_mem(&zipArchive, zipBuffer.data(), zipBuffer.size(), 0)) {
+                    return project_json;
+                }
+
+                // extract project.json
+                std::cout << "Extracting project.json..." << std::endl;
+                int file_index = mz_zip_reader_locate_file(&zipArchive, "project.json", NULL, 0);
+                if (file_index < 0) {
+                    return project_json;
+                }
+
+                size_t json_size;
+                const char *json_data = static_cast<const char *>(mz_zip_reader_extract_to_heap(&zipArchive, file_index, &json_size, 0));
+
+                // Parse JSON file
+                std::cout << "Parsing project.json..." << std::endl;
+                project_json = nlohmann::json::parse(std::string(json_data, json_size));
+                mz_free((void *)json_data);
+
+                // Image::loadImages(&zipArchive);
+                // mz_zip_reader_end(&zipArchive);
+            } else {
+                // if project is unzipped
+                file->clear();                 // Clear any EOF flags
+                file->seekg(0, std::ios::beg); // Go to the start of the file
+                (*file) >> project_json;
+            }
         }
-
-        // open ZIP file from the thing that we just did
-        std::cout<<"Opening SB3 file..."<<std::endl;
-        memset(&zipArchive,0,sizeof(zipArchive));
-        if (!mz_zip_reader_init_mem(&zipArchive,zipBuffer.data(),zipBuffer.size(),0)){
-            return project_json;
-        }
-
-        // extract project.json
-        std::cout<<"Extracting project.json..."<<std::endl;
-        int file_index = mz_zip_reader_locate_file(&zipArchive,"project.json",NULL,0);
-        if (file_index < 0){
-            return project_json;
-        }
-
-        size_t json_size;
-        const char* json_data = static_cast<const char*>(mz_zip_reader_extract_to_heap(&zipArchive, file_index, &json_size, 0));
-
-        // Parse JSON file
-        std::cout<<"Parsing project.json..."<<std::endl;
-        project_json = nlohmann::json::parse(std::string(json_data,json_size));
-        mz_free((void*)json_data);
-
-        //Image::loadImages(&zipArchive);
-        //mz_zip_reader_end(&zipArchive);
-    }
-    else {
-        // if project is unzipped
-    file->clear(); // Clear any EOF flags
-    file->seekg(0, std::ios::beg); // Go to the start of the file
-    (*file) >> project_json;
-}
-
-    return project_json;
-
+        return project_json;
     }
 
     static int openFile(std::ifstream *file);
