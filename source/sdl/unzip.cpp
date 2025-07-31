8b719e5a406ef71b9fde7cb57d8a8f346f6dda60
Grady Link <github@grady.link>
2025-07-30 20:45:44 -0700

    feat: get it to load the file

--- source/sdl/unzip.cpp
+++ source/sdl/unzip.cpp
@@ -30,6 +30,8 @@ int Unzip::openFile(std::ifstream *file) {
 
 #ifdef __WIIU__
         file->open("romfs:/" + filename, std::ios::binary | std::ios::ate);
+#elif defined __EMSCRIPTEN__
+        file->open(filename, std::ios::binary | std::ios::ate);
 #else
         file->open(filePath, std::ios::binary | std::ios::ate);
 #endif
