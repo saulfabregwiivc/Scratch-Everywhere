67b1c36d5295d6075c8137acc2280b9feb8e31f8
NateXS <thecoolgamer0003@gmail.com>
2025-08-06 14:31:16 -0500

    use `SYS_Report` for logging

--- source/scratch/os.cpp
+++ source/scratch/os.cpp
@@ -1,20 +1,38 @@
 #include "os.hpp"
+#ifdef __OGC__
+#include <gccore.h>
+#endif
 
 size_t MemoryTracker::totalAllocated = 0;
 size_t MemoryTracker::peakUsage = 0;
 size_t MemoryTracker::allocationCount = 0;
 
 void Log::log(std::string message, bool printToScreen) {
-    if (printToScreen)
+    if (printToScreen) {
+#ifdef __OGC__
+        SYS_Report("%s\n", message.c_str());
+#else
         std::cout << message << std::endl;
+#endif
+    }
 }
 void Log::logWarning(std::string message, bool printToScreen) {
-    if (printToScreen)
+    if (printToScreen) {
+#ifdef __OGC__
+        SYS_Report("Warning: %s\n", message.c_str());
+#else
         std::cout << "Warning: " << message << std::endl;
+#endif
+    }
 }
 void Log::logError(std::string message, bool printToScreen) {
-    if (printToScreen)
+    if (printToScreen) {
+#ifdef __OGC__
+        SYS_Report("Error: %s\n", message.c_str());
+#else
         std::cerr << "Error: " << message << std::endl;
+#endif
+    }
 }
 void Log::writeToFile(std::string message, std::string filePath) {
 }
